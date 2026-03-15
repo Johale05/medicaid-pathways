4cdbec23b90b281a16b05be37af1eb5310c9a903
Johale05 <johale@thehalelawfirm.com>
2026-03-14 19:53:51 -0500

    Clean up talk page duplicate chrome and spacing

--- app/(site)/talk/layout.tsx
+++ app/(site)/talk/layout.tsx
@@ -1,12 +1,3 @@
-import MinimalIdentityHeader from "@/components/MinimalIdentityHeader";
-import SiteFooter from "@/components/SiteFooter";
-
 export default function TalkLayout({ children }: { children: React.ReactNode }) {
-  return (
-    <div className="min-h-screen flex flex-col">
-      <MinimalIdentityHeader />
-      <main className="flex-1">{children}</main>
-      <SiteFooter />
-    </div>
-  );
+  return <>{children}</>;
 }
