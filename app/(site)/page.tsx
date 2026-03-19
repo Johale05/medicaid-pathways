a183de869af67c92490e0d95838bf3d7a9c3b28b
Johale05 <johale@thehalelawfirm.com>
2026-03-19 07:22:37 -0500

    Remove home pathway test video

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -10,7 +10,7 @@ export default function HomePage() {
     "medicare-ending",
     "qualify-medically",
     "spend-everything",
-    "sell-the-house",
+    "home",
     "too-much-income",
     "too-late",
   ];
