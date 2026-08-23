1053d831ef305ff94bf54cdebc228f8bf63b9601
Johale05 <johale@thehalelawfirm.com>
2026-08-23 11:59:53 -0500

    Default captions and hide video download

--- components/NativeVideoEmbed.tsx
+++ components/NativeVideoEmbed.tsx
@@ -12,8 +12,18 @@ export default function NativeVideoEmbed({ src, title, captions }: Props) {
   return (
     <div className="w-full overflow-hidden rounded-2xl border border-slate-200 shadow-soft bg-white">
       <div className="aspect-video w-full">
-        <video className="h-full w-full" src={src} title={title} crossOrigin="anonymous" controls playsInline>
-          {captions && <track kind="captions" src={captions.src} srcLang={captions.srcLang} label={captions.label} />}
+        <video
+          className="h-full w-full"
+          src={src}
+          title={title}
+          crossOrigin="anonymous"
+          controls
+          controlsList="nodownload"
+          playsInline
+        >
+          {captions && (
+            <track kind="captions" src={captions.src} srcLang={captions.srcLang} label={captions.label} default />
+          )}
         </video>
       </div>
     </div>
